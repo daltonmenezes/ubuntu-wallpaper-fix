40337cc1454f43eb0c8efbcd4bcf6b7939397bcb
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-20 09:18:26 -0200

    add options mode to ubuntu-wallpaper-fix.sh

--- ubuntu-wallpaper-fix.sh
+++ ubuntu-wallpaper-fix.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 running=true
+silent=false
+startup=false
 numberOfConnectedMonitors=`xrandr -q | grep ' connected' | wc -l`
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -s | --silent) silent=true;;
+    --onstartup) startup=true;;
+    *) echo "The '$1' argument doesn't exists. Beep..."; exit 0;;
+  esac; shift; shift
+done
+
 . $HOME/.ubuntu-wallpaper-fix/src/Fixer.sh
-sleep 6
-runFix
+
+if [ $startup = true ]; then
+  sleep 6
+  runFix > /dev/null
+fi
+
+if [ $silent == true ]; then
+  runFix > /dev/null
+else
+  runFix
+fi
