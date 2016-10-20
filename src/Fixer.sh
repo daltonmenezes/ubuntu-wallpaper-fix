48425b9c505ccfa7d08453c9c2580905f7b9534f
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-20 09:15:57 -0200

    move the fix to a function in Fixer.sh

---
+++ src/Fixer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+function runFix {
+  while $running; do
+    if [ "$numberOfConnectedMonitors" -ge "2" ]; then
+      printf 'Executing...'
+      if pgrep -f "gvfsd-trash" > /dev/null; then
+          rm -rf $HOME/.cache/wallpaper/*
+          gsettings set org.gnome.desktop.background picture-options stretched
+          gsettings set org.gnome.desktop.background picture-options zoom
+          printf '\nDone\n'
+          exit 0
+      fi
+    else
+      printf 'Just only one monitor is connected... Nothing to do here! Beep.\n'
+      exit 0
+    fi
+    sleep 1
+  done
+}
--- ubuntu-wallpaper-fix.sh
+++ ubuntu-wallpaper-fix.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 running=true
 numberOfConnectedMonitors=`xrandr -q | grep ' connected' | wc -l`
-while $running; do
-  if [ "$numberOfConnectedMonitors" -ge "2" ]; then
-    printf 'Executing...'
-    if pgrep -f "gvfsd-trash" > /dev/null; then
-        sleep 5
-        rm -rf /home/$USER/.cache/wallpaper/*
-        gsettings set org.gnome.desktop.background picture-options stretched
-        gsettings set org.gnome.desktop.background picture-options zoom
-        printf '\nDone\n'
-        exit 0
-    fi
-  else
-    printf 'Just only one monitor is connected... Nothing to do here! Beep.\n'
-    exit 0
-  fi
-  sleep 1
-done
+. $HOME/.ubuntu-wallpaper-fix/src/Fixer.sh
+sleep 6
+runFix
